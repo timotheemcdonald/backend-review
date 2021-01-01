f5445b33a47f1798df5e1cd07c06e2aa31f76104
timotheemcdonald <timotheemcdonald@gmail.com>
2021-01-01 13:53:06 -0500

    adding comments table successful rollback and migration to latest

--- migrations/20201231205420_create-users-table.js
+++ migrations/20201231205420_create-users-table.js
@@ -1,50 +1,75 @@
 
-exports.up = function(knex) {
+exports.up = function (knex) {
   return knex.schema.createTable('users', tbl => {
-      tbl.increments()
-      tbl.string('username', 12)
+    tbl.increments()
+    tbl.string('username', 12)
       .notNullable()
       .unique()
-      tbl.string('password', 12)
+    tbl.string('password', 12)
       .notNullable()
-      tbl.timestamps(true, true)
+    tbl.date('created_on')
   })
-  .createTable('posts', tbl => {
+    .createTable('posts', tbl => {
       tbl.increments()
       tbl.string('title')
-      .notNullable()
-      tbl.text('postContent')
-      .notNullable()
+        .notNullable()
+      tbl.text('post')
+        .notNullable()
       tbl.timestamps(true, true)
-    //foreign key to users
+      //foreign key to users
       tbl.integer('user_id')
-      .unsigned()
-      .references('id')
-      .inTable('users')
-      .onDelete('CASCADE')
-      .onUpdate('CASCADE')
-  })
-  .createTable('profile', tbl => {
+        .notNullable()
+        .unsigned()
+        .references('id')
+        .inTable('users')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+    })
+    .createTable('profile', tbl => {
       tbl.increments()
       tbl.string('profileName')
       tbl.text('bio')
       //foreign key to users
       tbl.integer('user_id')
-      .unsigned()
-      .references('id')
-      .inTable('users')
-      .onDelete('CASCADE')
-      .onUpdate('CASCADE')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('users')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
       //foreign key to posts
       tbl.integer('post_id')
-      .unsigned()
-      .references('id')
-      .inTable('posts')
-      .onDelete('CASCADE')
-      .onUpdate('CASCADE')
-  })
+        .notNullable()
+        .unsigned()
+        .references('id')
+        .inTable('posts')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+    })
+    .createTable('comments', tbl => {
+      tbl.increments()
+      tbl.text('comment')
+        .notNullable()
+      tbl.timestamps(true, true)
+      //foreign key to users
+      tbl.integer('user_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('users')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+      //foreignkeytoposts
+      tbl.integer('post_id')
+        .notNullable()
+        .unsigned()
+        .references('id')
+        .inTable('posts')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+    })
 };
 
-exports.down = function(knex) {
-  return knex.schema.dropTableIfExists('profile').dropTableIfExists('posts').dropTableIfExists('users')
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('comments').dropTableIfExists('profile').dropTableIfExists('posts').dropTableIfExists('users')
 };
