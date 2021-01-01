d765ec28be4a197884b5f20a39c0e781db79e675
timotheemcdonald <timotheemcdonald@gmail.com>
2021-01-01 00:03:18 -0500

    adding get and post

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const Users = require('./models/dbHelpers')
 
 const server = express()
 
@@ -8,6 +9,26 @@ const PORT = 5000
 
 server.get('/', (req, res) => {
     res.json({ message: 'Server Up and Running'})
+})
+
+server.post('/api/users', (req, res) => {
+    Users.add(req.body)
+    .then(user => {
+        res.status(200).json(user)
+    })
+    .catch(error => {
+        res.status(500).json({ message: 'cannot add user'})
+    })
+})
+
+server.get('/api/users', (req, res) => {
+    Users.find()
+    .then(users => {
+        res.status(200).json(users)
+    })
+    .catch(error => {
+        res.status(500).json({message: 'Unable to find Users'})
+    })
 })
 
 server.listen(PORT, () => {
--- models/dbHelpers.js
+++ models/dbHelpers.js
@@ -3,6 +3,17 @@ const knex = require('knex')
 const config = require('../knexfile')
 const db = knex(config.development)
 
-function add(user){
-    
+module.exports = {
+    add,
+    find
+}
+
+async function add(user){
+   const [id] = await db('users').insert(user)
+
+   return id
+}
+
+function find() {
+    return db('users')
 }
